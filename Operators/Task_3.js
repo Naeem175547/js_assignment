826e75a934745541f8b5f1587227cd7e3218ac13
Naeem175547 <144101668+Naeem175547@users.noreply.github.com>
2023-11-22 19:55:26 +0530

    Add files via upload

---
+++ Operators/Task_3.js
@@ -0,0 +1,2 @@
+let x=0;
+x==0?console.log("x is zero"):x>0?console.log("x is positive"):console.log("x is negative");
---
+++ Operators/Task_4.js
@@ -0,0 +1,2 @@
+let person_age=17;
+person_age>=18?console.log("You can vote"):console.log("you cannot vote");
